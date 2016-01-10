b2414932378fce844a6bcb695ee77c957be648a8
jriviere <juliette.riviere@etu.u-bordeaux.fr>
2016-01-10 21:52:34 +0100

    ajout champ descritpion

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160110203710) do
+ActiveRecord::Schema.define(version: 20160110205204) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title",       limit: 255
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20160110203710) do
     t.integer  "id_author",   limit: 4
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
+    t.text     "description", limit: 65535
   end
 
 end
